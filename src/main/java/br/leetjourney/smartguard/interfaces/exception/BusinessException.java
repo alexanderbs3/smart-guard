6627e2bae3c353a8f3c4fee10c07071082697a08
Alexander <alexander.cbss@hotmail.com>
2026-05-16 07:47:58 -0300

    refactor(api): remove legacy exception handling

--- src/main/java/br/leetjourney/smartguard/interfaces/exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package br.leetjourney.smartguard.interfaces.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
